75676fbec4130b7c79283351ce7ed808adf1d4fe
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-07-17 09:30:35 +0200

    register inputs

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,58 +1,119 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import style from "./FormSection.module.css";
 
-import style from './FormSection.module.css'
+type MortageType = "repayment" | "mortageOnly";
+type Inputs = {
+	amount: number;
+	term: number;
+	rate: number;
+	mortgageType: {
+		value: MortageType,
+		checked: boolean,
+	},
+
+};
+
 
 const FormSection = () => {
-  return (
-	<section className={style.mainContainer}>
-		<form action="">
-			<h1>Mortgage Calculator</h1>
-			<button className={style.clearAllButton}>Clear All</button>
-			
-			<br />
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-			<label htmlFor="">Mortgage Amount</label>
-			<div className={`${style.amountField} ${style.inputAndPlacholderWrapper}`}>
-				<div className={`${style.placeholder} ${style.amountPlaceholder}`}>£</div>
-				<input type="number" name='amount' />
-			</div>
+	return (
+		<section className={style.mainContainer}>
+			<form action="" onSubmit={handleSubmit(onSubmit)}>
+				<h1>Mortgage Calculator</h1>
+				<button className={style.clearAllButton}>Clear All</button>
 
+				<br />
 
-			<div className={style.termRateWrapper}>
-				<div>
-					<label htmlFor="term">Mortgage Term</label>
-					<div className={`${style.amountField} ${style.inputAndPlacholderWrapper}`}>
-						<input type="number" name='term'/>
-						<div className={`${style.placeholder} ${style.amountPlaceholder}`}>years</div>
+				<label htmlFor="">Mortgage Amount</label>
+				<div
+					className={`${style.amountField} ${style.inputAndPlacholderWrapper}`}
+				>
+					<div
+						className={`${style.placeholder} ${style.amountPlaceholder}`}
+					>
+						£
+					</div>
+					<input
+						{...register("amount", { required: true })}
+						type="number"
+					/>
+				</div>
+
+				<div className={style.termRateWrapper}>
+					<div>
+						<label htmlFor="term">Mortgage Term</label>
+						<div
+							className={`${style.amountField} ${style.inputAndPlacholderWrapper}`}
+						>
+							<input
+								{...register("term", { required: true })}
+								type="number"
+							/>
+							<div
+								className={`${style.placeholder} ${style.amountPlaceholder}`}
+							>
+								years
+							</div>
+						</div>
+					</div>
+
+					<div>
+						<label htmlFor="">Interest Rate</label>
+						<div
+							className={`${style.amountField} ${style.inputAndPlacholderWrapper}`}
+						>
+							<input
+								{...register("rate", { required: true })}
+								type="number"
+							/>
+							<div
+								className={`${style.placeholder} ${style.amountPlaceholder}`}
+							>
+								%
+							</div>
+						</div>
 					</div>
 				</div>
 
-				<div>
-					<label htmlFor="">Interest Rate</label>
-					<div className={`${style.amountField} ${style.inputAndPlacholderWrapper}`}>
-						<input type="number" name='rate'/>
-						<div className={`${style.placeholder} ${style.amountPlaceholder}`}>%</div>
-					</div>
+				<div className={style.radioButtons}>
+					Mortgage Type
+					<label
+						className={`${style.repaymentLabel} ${style.radioLabel}`}
+						htmlFor="repayment"
+					>
+						<input
+							{...register("mortgageType")}
+							type="radio"
+							value="repayment"
+							id="repayment"
+							checked
+						/>
+						Repayment
+					</label>
+					<label className={style.radioLabel} htmlFor="interestOnly">
+						<input
+							{...register("mortgageType")}
+							type="radio"
+							value="interestOnly"
+							id="interestOnly"
+						/>
+						Interest Only
+					</label>
 				</div>
-			</div>
 
-			<div className={style.radioButtons} htmlFor="mortgageType">
-				Mortgage Type
-				<label className={`${style.repaymentLabel} ${style.radioLabel}`} htmlFor="repayment">
-					<input type="radio" value="repayment" name='mortgageType' id='repayment' checked/>
-					Repayment
-				</label>
+				<button className={style.calcButton}>
+					Calculate Repayment
+				</button>
+			</form>
+		</section>
+	);
+};
 
-				<label className={style.radioLabel} htmlFor="interestOnly">
-					<input type="radio" value="interestOnly" name='mortgageType' id='interestOnly' />
-					Interest Only
-				</label>
-			</div>
-
-			<button className={style.calcButton}>Calculate Repayment</button>
-
-		</form>
-	</section>
-  )
-}
-
-export default FormSection
+export default FormSection;
