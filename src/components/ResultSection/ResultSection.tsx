ca2faf2c75e0211b0777c32ea2763ae8c5863f8c
Simon A.D. Winter <112318684+SimonADW@users.noreply.github.com>
2024-08-06 15:00:57 +0200

    Display illustration when no calculations

--- src/components/ResultSection/ResultSection.tsx
+++ src/components/ResultSection/ResultSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import style from './ResultSection.module.css'
+import illustrationEmpty from '../../assets/images/illustration-empty.svg'
 
 type ResultProps = {
   mortgageCalculations: string[]
@@ -9,21 +10,34 @@ const ResultSection = ({mortgageCalculations}: ResultProps) => {
   
   return (
 	<>
-  <section className={style.resultSection}>
-    <h1>Your results</h1>
-    <p>
-      Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.
-    </p>
-    <div className={style.displayResultSectionBackground}>
-      <div className={style.displayResultSection}>
-        <p>Your monthly repayments</p>
-        <h3 className={style.monthlyRepay}>{mortgageCalculations[0]}</h3>
-        <hr />
-        <p>Total you´ll repay over the term</p>
-        <h3 className={style.termRepay}>{mortgageCalculations[1]}</h3>
+  {
+    // If calculations are done display results, else display illustration
+    mortgageCalculations[0] != "0" ?
+  
+    <section className={style.resultSection}>
+      <h1>Your results</h1>
+      <p>
+        Your results are shown below based on the information you provided. To adjust the results, edit the form and click "calculate repayments" again.
+      </p>
+      <div className={style.displayResultSectionBackground}>
+        <div className={style.displayResultSection}>
+          <p>Your monthly repayments</p>
+          <h3 className={style.monthlyRepay}>{mortgageCalculations[0]}</h3>
+          <hr />
+          <p>Total you´ll repay over the term</p>
+          <h3 className={style.termRepay}>{mortgageCalculations[1]}</h3>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+
+    :
+
+  <section className={`${style.resultSection} ${style.illustrationSection}`}>
+      <img src={illustrationEmpty} alt="Illustration" />
+      <h1>Results shown here</h1>
+      <p>Complete the form and click "Calculate payments to see what your monthly repayments would be</p>
+    </section>
+  }
   </>
   )
 }
